ca47b942c537d60921a73cf05684e9da9c631a29
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-25 19:24:48 +0200

    added array sorting

--- 2018-10-25/arrays.php
+++ 2018-10-25/arrays.php
@@ -3,5 +3,11 @@
 $arr1 = array("BMW", "Audi", "Mercedes");
 $arr2 = [];
 
-echo($arr1);
+//echo($arr1); //simulacija greske
+sort($arr1); // sortiraj od najmanjeg do najveceg
+var_dump($arr1);
+echo("<br>");
+rsort($arr1); //sort od najveceg prema najmanjem
+var_dump($arr1);
+echo("<br>");
 ?>
