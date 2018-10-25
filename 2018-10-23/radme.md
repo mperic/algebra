ea4a4a905684bd11aa2915492842a78b152f2977
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-25 18:57:27 +0200

    Variables, arrays and boolean

--- 2018-10-23/radme.md
+++
@@ -1,4 +0,0 @@
-Prvo predavanje
-
-HTML i CSS osnove
-XAMPP instalacija i pokretanje
---
+++ 2018-10-25/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$a = 5; // Kreiramo varijablu i dodajemo joj vrijednost
+$b = 10;
+$c = $a + $b; // zbrajamo dvije vrijednosti
+
+echo($c); //  Ispis varijable
+echo("<br>"); // ispis novog reda
+
+$name = "Sasa"; // Kreiramo varijablu i dodajemo joj string podataka
+echo($name);
+echo("<br>");
+var_dump($name); // var dump funkcija prikazuje sve informacije o varijabli
+echo("<br>");
+
+$array = [19, 48, 96]; // kreiramo polje
+var_dump($array);
+echo("<br>");
+echo($array[2]); // pristup elementu polja po indeksu
+// echo $array[3] // pristup elementu koji ne postoji
+echo("<br>");
+
+echo("Modulo: ");
+echo(9 % 7); // modulo
+
+echo("<br>");
+echo("Boolean");
+echo("<br>");
+
+echo("3==3 " . (3==3)); // usporedba
+echo("<br>");
+
+echo("3=='3' " . (3=="3")); // usporedba
+echo("<br>");
+
+echo("3===3 " . (3===3)); // usporedba
+echo("<br>");
+
+echo("3==='3' " . (3==="3")); // usporedba
+echo("<br>");
+?>
