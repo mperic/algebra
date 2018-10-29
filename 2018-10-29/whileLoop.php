1c327e0522385cea7323116866e0d507ea95aa99
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 18:38:52 +0100

    while example with infinite loop

---
+++ 2018-10-29/whileLoop.php
@@ -0,0 +1,13 @@
+<?php
+
+// While petlja primjer
+$number = 10;
+while ($number < 20) {
+  echo($number . "<br>");
+  $number++;
+}
+
+// Beskonacna petlja
+while ($number < 100) {
+  echo($number . " ");
+}
