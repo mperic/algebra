fc0a850dd0c7d995071b1b63f12419dc6f49f70f
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 18:07:49 +0100

    Added fibonnaci sequence

--- 2018-10-29/loops.php
+++ 2018-10-29/loops.php
@@ -4,13 +4,27 @@ for ($i = 0; $i < 10; $i++) {
   echo($i . "<br>");
 }
 
-echo "<hr>";
+echo "<hr> Parni do 100:  ";
 
 // Zadatak ispisi sve parne brojeve od 1 do 100
 for ($i = 1; $i <= 100; $i++) {
   if ($i%2==0) {
     echo($i . ", ");
   }
+}
+
+// Fibonnaci sequence
+echo "<hr> Fibonacci:  ";
+$firstNumber = 0;
+$secondNumber = 1;
+echo "0, 1, ";
+
+for ($i = 0; $i<10; $i++) {
+  $thirdNumber = $firstNumber + $secondNumber;
+  echo($thirdNumber . ", ");
+
+  $firstNumber = $secondNumber;
+  $secondNumber = $thirdNumber;
 }
 
 ?>
