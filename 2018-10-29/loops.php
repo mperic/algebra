e445102e5e8c9cf0f30faa0816c1efa0ba949297
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 18:09:56 +0100

    Multiplication table example

--- 2018-10-29/loops.php
+++ 2018-10-29/loops.php
@@ -27,4 +27,14 @@ for ($i = 0; $i<10; $i++) {
   $secondNumber = $thirdNumber;
 }
 
+// Tablica mnozenja 10x10
+echo "<hr>";
+for ($i = 1; $i<=10; $i++) {
+  for ($j = 1; $j<=10; $j++) {
+    echo(($i * $j) . " ");
+  }
+  echo "<br>";
+}
+
 ?>
+
