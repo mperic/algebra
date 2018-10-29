0da4213c4bf293575c2007cca3cca27729ad1fd6
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 18:49:29 +0100

    Added while example - odd numbers

---
+++ 2018-10-29/doWhileLoop.php
@@ -0,0 +1,9 @@
+<?php
+
+// Do while petlja
+
+$number = 10;
+do {
+  echo ($number);
+  $number--;
+} while ($number > 0);
--- 2018-10-29/whileLoop.php
+++ 2018-10-29/whileLoop.php
@@ -8,6 +8,16 @@ while ($number < 20) {
 }
 
 // Beskonacna petlja
-while ($number < 100) {
-  echo($number . " ");
+//while ($number < 100) {
+//  echo($number . " ");
+// }
+
+// ispis svih neparnih do 100
+echo("<hr>");
+$i = 1;
+while ($i<100) {
+  if ($i % 2 != 0) {
+    echo($i . " ");
+  }
+  $i++;
 }
