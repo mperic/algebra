b1e760b93f31cee8725805be1908c869e30bd525
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 19:52:34 +0100

    Added example with break statement

---
+++ 2018-10-29/associativeArrays.php
@@ -0,0 +1,43 @@
+<?php
+
+$ivanData = array(
+  "id"=>1, 
+  "firstName"=>"Ivan",
+  "lastName"=>"Ivic");
+
+$peroData = array(
+  "id"=>2, 
+  "firstName"=>"Pero",
+  "lastName"=>"Peric");
+
+$markoData = array(
+  "id"=>3, 
+  "firstName"=>"Marko",
+  "lastName"=>"Markic");
+
+$people = [$peroData, $ivanData, $markoData];
+
+//var_dump($people);
+
+foreach ($people as $person) {
+  echo(
+    "ID: " . $person["id"] . 
+    " First name: " . $person["firstName"] .
+    " Last name: " .$person["lastName"] .
+    "<br>"
+  );
+}
+
+//Zadatak 1 - ispisi sve do Ivana
+foreach ($people as $person) {
+  if ($person["firstName"] == "Ivan") {
+    break;
+  }
+
+  echo(
+    "ID: " . $person["id"] . 
+    " First name: " . $person["firstName"] .
+    " Last name: " .$person["lastName"] .
+    "<br>"
+  );
+}
