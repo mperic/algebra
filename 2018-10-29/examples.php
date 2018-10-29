b4e0eff99b45d5ee0d290e18a47f6dac509ce200
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-29 18:59:58 +0100

    For loop and array example

---
+++ 2018-10-29/examples.php
@@ -0,0 +1,12 @@
+<?php
+
+// Napravi listu/polje od 10 elemenata
+// Ispisi ih pomocu FOR petlje.
+
+$array = [5,10,15,7,17,22,34,88,99,100];
+
+for ($i = 0; $i<10; $i++) {
+  echo($array[$i] . ", ");
+}
+
+?>
