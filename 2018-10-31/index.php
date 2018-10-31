e1553ca12d002d779e19b41aefe66213a8c60e88
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-31 19:23:20 +0100

    Require and include

---
+++ 2018-10-31/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Include & Require examples
+
+//include "missingFile.php";
+// include_once
+
+//require "missingFile.php";
+//require_once
+
+// echo "ABC";
+
+require "examples1.php";
+
+drawSquare(10);
