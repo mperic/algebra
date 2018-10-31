2852b8f9c8c53ae7d750aa465c17bed6a9824235
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-31 17:57:08 +0100

    Finishing scope examples

--- 2018-10-31/functionScope.php
+++ 2018-10-31/functionScope.php
@@ -10,10 +10,16 @@ echo $s; // Undefined variable
 
 function scopeTest2($a) {
   $a = 10;
-  echo $a;
+  return $a;
 }
 
 $a = 5;
 echo "a: " . $a . "<br>";
 echo "function: " . scopeTest2($a) . "<br>";
 echo "a: " . $a . "<br>";
+
+function scopeTest3() {
+  echo $a;
+}
+
+scopeTest3();
