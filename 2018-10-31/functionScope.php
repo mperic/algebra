fbe4ee72ee211b76a53fd35697e4d1422b78e236
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-31 18:52:49 +0100

    added scope examples with global and static

--- 2018-10-31/functionScope.php
+++ 2018-10-31/functionScope.php
@@ -6,7 +6,7 @@ function scopeTest1($s) {
 
 }
 
-echo $s; // Undefined variable
+// echo $s; // Undefined variable
 
 function scopeTest2($a) { // ovaj "a" nije isti kao a u globalnom dijelu skripte
   $a = 10;
@@ -19,7 +19,7 @@ echo "function: " . scopeTest2($a) . "<br>";
 echo "a: " . $a . "<br>";
 
 function scopeTest3() {
-  echo $a; // nije moguce
+  // echo $a; // nije moguce
 }
 
 scopeTest3();
@@ -33,3 +33,28 @@ $abc = 5;
 echo "Before: " . $abc;
 scopeTest4($abc);
 echo "After: " . $abc;
+
+function scopeTest5() {
+  global $abc;
+  echo "ABC: " . $abc;
+}
+
+echo "<br>";
+echo "scope5 " . scopeTest5();
+
+function scopeTest6() {
+  return $GLOBALS['abc'];
+}
+
+echo "<br>";
+echo "scope6 " . scopeTest6() . "<br>";
+
+function scopeTest7() {
+  static $n = 5;
+  echo "n1: " . $n . "<br>";
+  $n++;
+  echo "n2: " . $n . "<br>";
+}
+
+scopeTest7();
+scopeTest7();
