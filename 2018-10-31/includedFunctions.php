11b05efa50e76e2a3ed831016353e822e5b1601e
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-10-31 19:13:18 +0100

    Added examples of built in fuctinos

---
+++ 2018-10-31/includedFunctions.php
@@ -0,0 +1,14 @@
+<?php
+
+// primjeri ugradenih funkcija u PHP
+
+echo "time=" . time() ."<br>";
+
+echo "strlen()=" . strlen("Pliva patka preko Save <br>");
+
+echo "date()=" . date("d m Y") . "<br>";
+
+echo "str_repeat()=" . str_repeat("paradajz", 2) . "<br>";
+
+$arr = [5,10,15];
+echo "sizeof()=" . sizeof($arr) . "<br>";
