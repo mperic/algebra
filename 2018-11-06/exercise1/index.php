bedb9b710669d43633bbdde724bc0321c9f08714
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-11-06 18:21:03 +0100

    Added html formating

---
+++ 2018-11-06/exercise1/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$filePath = "data.txt";
+$handle = fopen($filePath, "r");
+?>
+
+<table>
+  <tr><th>Osobe</th></tr>
+  <tr>
+    <td>ID</td>
+    <td>Ime</td>
+    <td>Prezime</td>
+    <td>Godina rodenja</td>
+    <td>Dob</td>
+  </tr>
+
+<?php
+
+while (($line = fgets($handle)) !== false) {
+  $dataArray = explode(",", $line);
+  echo "<tr>";
+  foreach($dataArray as $data) {
+    echo "<td>".$data."</td>";
+  }
+  echo "</tr>";
+}
+?>
+
+</table>
