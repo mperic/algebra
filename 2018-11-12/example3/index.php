c32f233eb138e7e1faef03a5c044ce71ef44f0dd
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-11-12 19:39:25 +0100

    Example 3 -licence plate formating

---
+++ 2018-11-12/example3/index.php
@@ -0,0 +1,21 @@
+<?php
+
+function formatRegistrationPlate($plate) {
+  if (strlen($plate) == 10) {
+
+    if (strpos($plate, " ")) {
+      return str_replace(" ", "#", $plate);
+    } else if (strpos($plate, "-")) {
+      return str_replace("-", "#", $plate);
+    }
+  } else if (strlen($plate) == 8) {
+    return substr($plate,0,2) . "#" . substr($plate,2,4) . "#" . substr($plate,5,2);
+  } else {
+    die("krivi unos");
+  }
+}
+
+echo "ZG 1234 XY = " . formatRegistrationPlate("ZG 1234 XY") . "<br>";
+echo "ZG-1234-XY = " . formatRegistrationPlate("ZG-1234-XY") . "<br>";
+echo "ZG1234XY = " . formatRegistrationPlate("ZG1234XY") . "<br>";
+echo "ZG-1234-XY PERO = " . formatRegistrationPlate("ZG-1234-XY PERO") . "<br>";
