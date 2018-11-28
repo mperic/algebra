25cd4a6f4fdfbe2dd3bb2715a657cc78cc8b706a
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-11-28 18:16:22 +0100

    Echoing all car details

--- 2018-11-28/index.php
+++ 2018-11-28/index.php
@@ -2,4 +2,9 @@
 
 require_once("model/Car.php");
 
-$c1 = new Car();
+$c1 = new Car("Kia", "Stinger", "2018", "150");
+
+echo "<p> {$c1->getManufacturer()} </p>";
+echo "<p> {$c1->getModel()} </p>";
+echo "<p> {$c1->getYear()} </p>";
+echo "<p> {$c1->getKW()} </p>";
