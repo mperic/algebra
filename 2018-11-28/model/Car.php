89951f2a68a861aaa9ff853badd11a6d093d3aae
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2018-11-28 18:06:41 +0100

    Created index and Car model

---
+++ 2018-11-28/model/Car.php
@@ -0,0 +1,31 @@
+<?php
+
+class Car {
+  private $manufacturer;
+  private $model;
+  private $year;
+  private $kW;
+
+  public __construct($man, $model, $year, $kW) {
+    $this->manufacturer = $man;
+    $this->model = $model;
+    $this->year = $year;
+    $this->kW = $kW;
+  }
+
+  public function getManufacturer() {
+    return $this->manufacturer;
+  }
+
+  public function getModel() {
+    return $this->model;
+  }
+
+  public function getKw() {
+    return $this->kW;
+  }
+
+  public function getYear() {
+    return $this->year;
+  }
+}
